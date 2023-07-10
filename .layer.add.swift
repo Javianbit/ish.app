19d0293814c270d2f4bc661b1e99cb57c83be0c3
Javianbit <74882906+Javianbit@users.noreply.github.com>
2023-07-10 15:39:51 -0500

    push-202307101537.log

---
+++ .layer.add.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class ViewController : UIViewController {
+
+override func viewDidLoad() {
+
+super.viewDidLoad()
+
+let activity = UIActivityIndicatorView(frame: .zero)
+
+activity.layer.add(CAAnimation(), forKey: "")
+
+}
+
+}
